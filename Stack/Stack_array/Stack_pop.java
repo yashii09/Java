6777db5cf1cc8f02cddf1a606baaeffd3fc4e6fe
yashi-cap <yashasvisingh259@gmail.com>
2025-02-15 15:52:05 +0530

    added stack code

--- Stack/Stack_array/Stack_pop.java
+++ Stack/Stack_array/Stack_pop.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-public class Stack_push {
+public class Stack_pop {
     int MAX = 1000;
     int top;
-    int stack[] = new int[MAX]; // maximum size of stack
+    int stack[] = new int[MAX];
 
     boolean isEmpty() {
         return (top < 0);
     }
 
-    Stack_push() {
+    Stack_pop() {
         top = -1;
     }
 
@@ -19,8 +19,17 @@ public class Stack_push {
             return false;
         } else {
             stack[++top] = x;
-            System.out.println(x + " pushed into stack");
             return true;
+        }
+    }
+
+    int pop() {
+        if (top < 0) {
+            System.out.println("Stack underflow");
+            return 0;
+        } else {
+            int x = stack[top--];
+            return x;
         }
     }
 
@@ -29,24 +38,29 @@ public class Stack_push {
             System.out.println("Stack is empty");
             return;
         }
-        System.out.println("Stack elements:");
+        System.out.println("Stack elements: ");
         for (int i = top; i >= 0; i--) {
             System.out.println(stack[i]);
         }
     }
 
     public static void main(String args[]) {
-        Stack_push s = new Stack_push();
+        Stack_pop s = new Stack_pop();
 
+        System.out.println("Enter the number of element in stack: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of element of stack: ");
         int n = sc.nextInt();
 
-        System.out.println("Enter the element of stack: ");
+        System.out.println("Enter the elements of stack: ");
         for (int i = 0; i < n; i++) {
             int ele = sc.nextInt();
             s.push(ele);
         }
+        System.out.println("Elements present in stack: ");
         s.printStack();
+        System.out.println("Element " + s.pop() + " Popped from stack");
+
+        s.printStack();
+
     }
 }
